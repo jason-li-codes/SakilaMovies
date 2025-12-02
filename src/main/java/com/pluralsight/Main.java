281a76eda3c86449f07ac74a627c342610079eb9
jasonli <lijason991@gmail.com>
2025-12-02 10:39:16 -0500

    completed main menu and printResults

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,134 @@
 package com.pluralsight;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.nio.file.LinkPermission;
+import java.sql.*;
+import java.util.Scanner;
+
 public class Main {
+
+    public static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // requires we pass in a username and password
+        if (args.length != 2) {
+            // displays a message to the user
+            System.out.println("Application requires username and password to run database.");
+            // exits the app if args[] is not correct input
+            System.exit(0);
+        }
+
+        // gets the username and password from args[]
+        String username = args[0];
+        String password = args[1];
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/sakila");
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        while (true) {
+
+            System.out.println("""
+                    What would you want to do?
+                        1) Display all actors
+                        2) Search by name
+                        3) Search by category
+                        0) Exit program
+                    """);
+
+            switch (input.nextInt()) {
+                case 1 -> displayAllActors(dataSource);
+                case 2 -> searchByName(dataSource);
+                case 3 -> searchByCategory(dataSource);
+                case 0 -> {
+                    System.out.println("EXITING PROGRAM...");
+                    System.exit(1);
+                }
+                default -> System.out.println("That is not a valid menu option, please try again.");
+            }
+
+        }
+    }
+
+    public static void printResults(ResultSet results) throws SQLException {
+
+        ResultSetMetaData metaData = results.getMetaData();
+        int columnCount = metaData.getColumnCount();
+
+        System.out.printf("|%-5s|%-45s|%-45s|\n", "Actor ID", "First Name", "Last Name");
+
+        while (results.next()) {
+            int actorID = results.getInt(1);
+            String firstName = results.getString(2);
+            String lastName = results.getString(3);
+
+            System.out.printf("|%-5s|%-45s|%-45s|\n", actorID, firstName, lastName);
+        }
+    }
+
+
+    public static void displayAllActors(BasicDataSource dataSource) {
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("""
+                     SELECT
+                        actor_id,
+                        first_name,
+                        last_name
+                     FROM
+                        actors
+                     ORDER BY
+                        actor_id
+                     """)) {
+            ResultSet results = preparedStatement.executeQuery();
+            printResults(results);
+        } catch (SQLException e) {
+            System.out.println("Error: could not retrieve information.");
+            System.exit(1);
+        }
+    }
+
+    public static void searchByName(BasicDataSource dataSource) {
+
+        System.out.println("What is the first name of the actor you're looking for?");
+        String inputFirstName = input.nextLine().trim();
+        System.out.println("What is the last name of the actor you're looking for?");
+        String inputLastName = input.nextLine().trim();
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("""
+                     SELECT
+                        actor_id,
+                        first_name,
+                        last_name
+                     FROM
+                        actors
+                     WHERE
+                        first_name LIKE ?
+                        AND
+                        last_name LIKE ?
+                     ORDER BY
+                        actor_id
+                     """)) {
+
+            preparedStatement.setString(1, "%" + inputFirstName + "%");
+            preparedStatement.setString(2, "%" + inputLastName + "%");
+
+            ResultSet results = preparedStatement.executeQuery();
+            printResults(results);
+        } catch (SQLException e) {
+            System.out.println("Error: could not retrieve information.");
+            System.exit(1);
+        }
+    }
+
+
+
+
+
+
+
 }
